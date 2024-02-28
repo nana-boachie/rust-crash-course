9cb31dbfc89a51bab143af2b857773118916da95
Nana Kwame Yiadom Boachie <yiadom_deb@yiadom.net>
2024-02-28 19:46:19 +0000

    Hi I'm learning rust

---
+++ rust-crash-course/target/debug/deps/rust_crash_course-f25ab38bf6d66291.d
@@ -0,0 +1,5 @@
+/home/yiadom_deb/Documents/Rust/rust-crash-course/rust-crash-course/target/debug/deps/librust_crash_course-f25ab38bf6d66291.rmeta: src/main.rs
+
+/home/yiadom_deb/Documents/Rust/rust-crash-course/rust-crash-course/target/debug/deps/rust_crash_course-f25ab38bf6d66291.d: src/main.rs
+
+src/main.rs:
---
+++ rust-crash-course/target/debug/deps/rust_crash_course-f61d3813a61242f5.d
@@ -0,0 +1,5 @@
+/home/yiadom_deb/Documents/Rust/rust-crash-course/rust-crash-course/target/debug/deps/librust_crash_course-f61d3813a61242f5.rmeta: src/main.rs
+
+/home/yiadom_deb/Documents/Rust/rust-crash-course/rust-crash-course/target/debug/deps/rust_crash_course-f61d3813a61242f5.d: src/main.rs
+
+src/main.rs:
